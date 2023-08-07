a06eafd0aca144b59bb30c5366c63cd173b2296b
patnaikabhishek <141665391+patnaikabhishek@users.noreply.github.com>
2023-08-07 22:50:11 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ## Data Science Languages
+# 
+# Some of the popular languages that Data Scientists use are:
+# 
+# - Python
+# - R
+# - Julia
+# - SQL
+
+# ## Data Science Libraries
+# 
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# - NumPy
+# - Pandas
+# - Matplotlib
+# - Seaborn
+# - Scikit-learn
+# - TensorFlow
+# - PyTorch
+
+# ## Data Science Tools
+# 
+# | Category      | Tools                |
+# |---------------|----------------------|
+# | Data Cleaning | OpenRefine, Trifacta |
+# | Data Analysis | Jupyter, RStudio    |
+# | Visualization | Tableau, Plotly      |
+# | Database      | MySQL, PostgreSQL   |
+# 
+
+# ### Arithmetic Expressions
+# 
+# Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[4]:
+
+
+# This is a simple arithmetic expression to multiply then add integers.
+result = (3 * 4) + 5
+print("Result:", result)
+
+
+# In[5]:
+
+
+# This will convert 200 minutes to hours by dividing by 60.
+minutes = 200
+hours = minutes / 60
+print("Hours:", hours)
+
+
+# ## Objectives
+# 
+# 
+# **Objectives:**
+# 
+# - List popular languages for Data Science.
+# - Explore essential Data Science libraries.
+# - Understand common Data Science tools and techniques.
+# - Perform basic arithmetic operations.
+# - Convert units of measurement in Python.
+# 
+
+# ## Author
+# 
+# Abhishek Patnaik
